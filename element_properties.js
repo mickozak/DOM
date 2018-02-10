923551fd6f5a4b3a3bc3b44b30157c5a884fedc0
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-10 20:25:22 +0100

    Create  element_properties.js

---
+++ element_properties.js
@@ -0,0 +1,8 @@
+//The most commonly used properties and methods.
+
+innerHTML	//sets or returns the HTML of the item.
+innerText	//sets or returns the text in the element (without html).
+tagName	//returns the tag name.
+setAttribute	//sets the element's attribute.
+getAttribute	//gets the element's attribute.
+dataset //returns the dataset.
