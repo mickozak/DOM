c4bbd21c6144eb3eab5e299db2605cab5c629c16
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-11 14:44:45 +0100

    Create creating_and_deleting.js

---
+++ js/creating_and_deleting.js
@@ -0,0 +1,10 @@
+//Creating an object.
+
+//Creating an object with document.createElement() - for example:
+
+let objOne = document.createElement('div');
+    objOne.id = 'myObj'
+    objOne.innerText = 'This is...';
+    objOne.setAttribute('title', 'This is new Element');
+    objOne.classList.add('objElement');
+    objOne.style.color = 'red';
