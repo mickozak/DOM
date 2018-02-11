c26a66b6d25445f4421c3426bbadaa295c3df6c4
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-11 14:54:46 +0100

    Update creating_and_deleting.js

--- js/creating_and_deleting.js
+++ js/creating_and_deleting.js
@@ -1,19 +1,24 @@
 //Creating an object.
 
-//Creating an object with document.createElement() - for example:
+//Creating an object with document.createElement(newElement) - for example:
 
-let objOne = document.createElement('div');
-    objOne.id = 'myObj'
-    objOne.innerText = 'This is...';
-    objOne.setAttribute('title', 'This is new Element');
-    objOne.classList.add('objElement');
-    objOne.style.color = 'red';
+let newObj = document.createElement('div');
+    newObj.id = 'myObj'
+    newObj.innerText = 'This is...';
+    newObj.setAttribute('title', 'This is new Element');
+    newObj.classList.add('objElement');
+    newObj.style.color = 'red';
 
 //The element can be inserted into the document via parentElement.appendChild(newElement) - for example:
 
 let div = document.querySelector('.firstParagraph');
-    div.appendChild(objOne);
+    div.appendChild(newObj);
 
 //Removing elements via parentNode.removeChild(element) - for example:
 
-    div.removeChild(objOne);
+    div.removeChild(newObj);
+
+//Replacing elements via parent.replaceChild(newChild, oldChild) - for example:
+
+    div.replaceChild(newObj, oldObj);
+    
