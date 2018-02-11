f1d6948da583f9bc91e8182eb8ce107f24558e06
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-11 14:48:27 +0100

    Update creating_and_deleting.js

--- js/creating_and_deleting.js
+++ js/creating_and_deleting.js
@@ -8,3 +8,8 @@ let objOne = document.createElement('div');
     objOne.setAttribute('title', 'This is new Element');
     objOne.classList.add('objElement');
     objOne.style.color = 'red';
+
+//The element can be inserted into the document via parentElement.appendChild() - for example:
+
+ let div = document.querySelector('.firstParagraph');
+    div.appendChild(objOne);
