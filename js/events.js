62a175fef229197dd8f7c51ce05666f004f83ce4
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-17 19:01:43 +0100

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,1 +1,7 @@
 //Events are actions that happen in the browser. It can be called by the user, but also by every element on the page.
+
+//A new event registration mode - addEventListener().
+//It accepts 3 arguments: event type, callback function and optional argument that enables or disables event propagation.
+
+let element = document.querySelector('.elem');
+
