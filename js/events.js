4ed12872e228733f23183127961a8becddc007b6
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-17 19:08:39 +0100

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,7 +1,16 @@
 //Events are actions that happen in the browser. It can be called by the user, but also by every element on the page.
 
-//A new event registration mode - addEventListener().
+//A new event registration mode - .ddEventListener().
 //It accepts 3 arguments: event type, callback function and optional argument that enables or disables event propagation.
 
 let element = document.querySelector('.elem');
 
+element.addEventListener('click', showMe);
+
+//or
+
+element.addEventListener('click', function(){
+    this.style.color='red';
+});
+
+//.lement.removeEventListener() is used to de-register functions from a given event, it accepts the same arguments that were used to register the given event.
