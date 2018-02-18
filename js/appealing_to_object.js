a28f8983c879f76db07cd6724227f3cd9ff41e60
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-18 17:48:46 +0100

    Update appealing_to_object.js

--- js/appealing_to_object.js
+++ js/appealing_to_object.js
@@ -1,4 +1,4 @@
-//DOM (Document Object Model) - It is a model, interface that using our methods and properties allows us to act on our document.
+//DOM (Document Object Model) - it is a model, interface that using our methods and properties allows us to act on our document.
     
 //The DOMContentLoaded event guarantees that the script will start working when the entire DOM tree has been loaded.
 
@@ -16,8 +16,8 @@ document.querySelectorAll('');
 
 //getElementsByClassName i getElementByTagName - return so-called living collections in contrast to querySelectorAll.
 //If we place such a collection under a variable, it will automatically update depending on the elements on the page.
-//We try to save downloaded elements to a variable.
 
+//We try to save downloaded elements to a variable.
 //For example:
 
 let buttonDelete = document.querySelector('.buttonDelete');
