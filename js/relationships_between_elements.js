96461f47cd3fa7c69a47df19d5fe8e9bba26af75
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-26 22:05:45 +0200

    Update relationships_between_elements.js

--- js/relationships_between_elements.js
+++ js/relationships_between_elements.js
@@ -12,3 +12,9 @@ one.lastElementChild
 
 one.children; 
 one.children[0]
+
+one.firstElementChild.nextElementSibling 
+one.firstElementChild.nextSibling
+
+one.firstElementChild.previousElementSibling 
+one.firstElementChild.previousSibling 
