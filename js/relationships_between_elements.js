633d02445a78e6a1148a7e6494a4cf2187e1aeb8
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-26 21:58:08 +0200

    Create relationships_between_elements.js

---
+++ js/relationships_between_elements.js
@@ -0,0 +1,14 @@
+// Relationships between elements
+
+
+one.parentElement
+one.parentNode 
+
+one.firstChild 
+one.lastChild 
+
+one.firstElementChild 
+one.lastElementChild 
+
+one.children; 
+one.children[0]
