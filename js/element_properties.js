95a02ff4a0128735ee2c0b2ee48c650a9e1f4024
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-02-28 22:18:03 +0300

    Update element_properties.js

--- js/element_properties.js
+++ js/element_properties.js
@@ -8,5 +8,18 @@
 // removeAttribute - is used to delete the attribute.
 // dataset - returns the dataset.
 
-//For example:
+//For example: innerHTML, innertext or textContent (innerText returns the text after 
+//applying styles, and textContent suppresses styles).
 
+let btn = document.querySelector('.btn');
+
+console.log(btn.innerHTML); 
+console.log(btn.innerText); 
+console.log(btn.textContent);
+
+//or
+
+let div = document.querySelector('title');
+div.innerText='Animal';
+
+//For example: innerHTML, innertext or textContent.
