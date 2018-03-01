38983d0909776a8af6c01321bf7fcabf375cbbad
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-03-01 23:02:48 +0300

    Update script.js

--- js/appealing_to_object/script.js
+++ js/appealing_to_object/script.js
@@ -1,12 +1,12 @@
-//DOM (Document Object Model) - it is a model, interface that using our methods and properties allows us to act on our document.
+/*DOM (Document Object Model) - it is a model, interface that using our methods and properties allows us to act on our document.
     
-//The DOMContentLoaded event guarantees that the script will start working when the entire DOM tree has been loaded.
+The DOMContentLoaded event guarantees that the script will start working when the entire DOM tree has been loaded.
 
 document.addEventListener("DOMContentLoaded", function() {
     
 });
 
-//Appealing to objects
+Appealing to objects
     
 document.getElementById('');
 document.getElementsByTagName('');
@@ -14,12 +14,18 @@ document.getElementsByClassName('');
 document.querySelector('');
 document.querySelectorAll('');
 
-//getElementsByClassName i getElementByTagName - return so-called living collections in contrast to querySelectorAll.
-//If we place such a collection under a variable, it will automatically update depending on the elements on the page.
+GetElementsByClassName i getElementByTagName - return so-called living collections in contrast to querySelectorAll.
 
-//We try to save downloaded elements to a variable.
+If we place such a collection under a variable, it will automatically update depending on the elements on the page.
+
+We try to save downloaded elements to a variable*/
+
 //For example:
+
+document.addEventListener("DOMContentLoaded", function() {
 
 let liList = document.querySelectorAll('li');
     
 console.log(liList);
+    
+});
