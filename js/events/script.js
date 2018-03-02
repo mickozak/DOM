95838954126cdcc969c4f370f04dc3866607dc79
Michał Kozak <34646280+mickozak@users.noreply.github.com>
2018-03-02 19:53:15 +0000

    Update script.js

--- js/events/script.js
+++ js/events/script.js
@@ -3,13 +3,13 @@
 //A new event registration mode - .ddEventListener().
 //It accepts 3 arguments: event type, callback function and optional argument that enables or disables event propagation.
 
-let element = document.querySelector('.elem');
+let element = document.querySelector('.elem')
 
 function showMeIt(){
-    console.log('You clicked')
+    console.log('You clicked');
 }
 
-element.addEventListener('click', showMeIt);
+element.addEventListener('click',showMeIt);
 
 //or
 
@@ -17,20 +17,19 @@ element.addEventListener('click', function(){
     this.style.color='red';
 });
 
-//Avoid anonymous functions.
+/*Avoid anonymous functions.
 
-//.element.removeEventListener() is used to de-register functions from a given event, 
-//it accepts the same arguments that were used to register the given event.
+.element.removeEventListener() is used to de-register functions from a given event, 
+it accepts the same arguments that were used to register the given event.
 
 element.removeEventListener('click', showMeIt);
 
-//Calling up the event.
+Calling up the event.
 
 element.click()
 
-//Mouse events
+Mouse events:
 
-/*
 mousedown - the mouse button has been pressed
 mouseup - the mouse button has been released
 click - the mouse button has been pressed and released
@@ -38,10 +37,11 @@ dblclick - double click
 mousemove - the cursor moves around the element
 mouseover - the cursor is on the element
 mouseout - the cursor has left the elemen
-*/
 
 function showMeIt(){
     console.log('You clicked')
 }
 
 element.addEventListener('mousedown', showMeIt);
+
+*/
