0983b73b343ccf4be952692eb769b26ab4838217
Michał Kozak <kozak_m@outlook.com>
2018-03-20 16:59:20 +0100

    Update practical_example_one.js

--- js/practical_example/practical_example_one.js
+++ js/practical_example/practical_example_one.js
@@ -2,3 +2,14 @@ function setUpEvents(){
 
 var content = document.getElementById("content");
 var button = document.getElementById("show");
+  
+  button.onclick = function () {
+    
+    if (content.className = "open"){
+      content.classname="";
+      button.innerHTML="Show More";
+    } else {
+      content.classname="open";
+      button.innerHTML="Show Less";
+    }
+};
